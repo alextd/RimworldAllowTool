210ad5329259510148d29ab00adc3a5cfb82e077
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-20 19:23:24 -0700

    removing refill settings when referenced objects are removed

---
+++ Source/Patches/UrgentRefill_Deletion_Patches.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Harmony;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace AllowTool.Source.Patches
+{
+	[HarmonyPatch(typeof(HaulDestinationManager), "RemoveHaulDestination")]
+	class UrgentRefill_Deletion_Patches
+	{
+		//public void RemoveHaulDestination(IHaulDestination haulDestination)
+		public static void Postfix(IHaulDestination haulDestination)
+		{
+			if (haulDestination is ISlotGroupParent slotGroupParent &&
+				slotGroupParent.GetSlotGroup() is SlotGroup slotGroup)
+				AllowToolController.Instance.WorldSettings.MarkForRefill(haulDestination.Map, slotGroup, false);
+		}
+	}
+}
